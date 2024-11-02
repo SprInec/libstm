84f0bb9f40a39cfaa284adf37ed843e29932df04
SprInec <JulyCub@163.com>
2024-11-02 13:02:09 +0800

    modified bsp_led.h

--- Inc/bsp_led.h
+++ Inc/bsp_led.h
@@ -2,9 +2,10 @@
  * @file bsp_led.h
  * @author July (Email: JulyCub@163.com)
  * @brief The board support package for led.
- * @version 0.1
+ * @version v1.2
  * @date 2022.06.26
  * @updata 2023.07.15 - Add DevEBox-H743 LED support.
+ *         2024.11.02 - Add FreeRTOS support. Optimized grammatical.
  *
  * @copyright Copyright (c) 2023
  *
@@ -30,36 +31,86 @@ extern "C" {
 #define __BSP_LED2_Toggle() HAL_GPIO_TogglePin(BSP_LED_Port2, BSP_LED_PIN2)
 
 #if __RTOS_RTTHREAD_ENABLED
-#define __BSP_LED1_Ficker(__TIME__) ({	__BSP_LED1_Toggle(); 		\
-										rt_thread_mdelay(__TIME__); \
-										__BSP_LED1_Toggle(); 		\
-										rt_thread_mdelay(__TIME__); })
+#define __BSP_LED1_Ficker(__TIME__) \
+	do                              \
+	{                               \
+		__BSP_LED1_Toggle();        \
+		rt_thread_mdelay(__TIME__); \
+		__BSP_LED1_Toggle();        \
+		rt_thread_mdelay(__TIME__); \
+	} while (0)
 
-#define __BSP_LED2_Ficker(__TIME__) ({	__BSP_LED2_Toggle(); 		\
-										rt_thread_mdelay(__TIME__);	\
-										__BSP_LED2_Toggle(); 		\
-										rt_thread_mdelay(__TIME__); })
+#define __BSP_LED2_Ficker(__TIME__) \
+	do                              \
+	{                               \
+		__BSP_LED2_Toggle();        \
+		rt_thread_mdelay(__TIME__); \
+		__BSP_LED2_Toggle();        \
+		rt_thread_mdelay(__TIME__); \
+	} while (0)
+
+#define __BSP_LED3_Ficker(__TIME__) \
+	do                              \
+	{                               \
+		__BSP_LED3_Toggle();        \
+		rt_thread_mdelay(__TIME__); \
+		__BSP_LED3_Toggle();        \
+		rt_thread_mdelay(__TIME__); \
+	} while (0)
 #elif __RTOS_FREERTOS_ENABLED
-#define __BSP_LED1_Ficker(__TIME__) ({	__BSP_LED1_Toggle(); 		\
-										vTaskDelay(__TIME__); 	    \
-										__BSP_LED1_Toggle(); 		\
-										vTaskDelay(__TIME__); })
+#define __BSP_LED1_Ficker(__TIME__) \
+	do                              \
+	{                               \
+		__BSP_LED1_Toggle();        \
+		vTaskDelay(__TIME__);       \
+		__BSP_LED1_Toggle();        \
+		vTaskDelay(__TIME__);       \
+	} while (0)
 
-#define __BSP_LED2_Ficker(__TIME__) ({	__BSP_LED2_Toggle(); 		\
-										vTaskDelay(__TIME__); 	    \
-										__BSP_LED2_Toggle(); 		\
-										vTaskDelay(__TIME__); })
+#define __BSP_LED2_Ficker(__TIME__) \
+	do                              \
+	{                               \
+		__BSP_LED2_Toggle();        \
+		vTaskDelay(__TIME__);       \
+		__BSP_LED2_Toggle();        \
+		vTaskDelay(__TIME__);       \
+	} while (0)
+
+#define __BSP_LED3_Ficker(__TIME__) \
+	do                              \
+	{                               \
+		__BSP_LED3_Toggle();        \
+		vTaskDelay(__TIME__);       \
+		__BSP_LED3_Toggle();        \
+		vTaskDelay(__TIME__);       \
+	} while (0)
 #else
-#define __BSP_LED1_Ficker(__TIME__) ({	__BSP_LED1_Toggle(); \
-										delay_ms(__TIME__);  \
-										__BSP_LED1_Toggle(); \
-										delay_ms(__TIME__); })
+#define __BSP_LED1_Ficker(__TIME__) \
+	do                              \
+	{                               \
+		__BSP_LED1_Toggle();        \
+		delay_ms(__TIME__);         \
+		__BSP_LED1_Toggle();        \
+		delay_ms(__TIME__);         \
+	} while (0)
 
-#define __BSP_LED2_Ficker(__TIME__) ({	__BSP_LED2_Toggle(); \
-										delay_ms(__TIME__);  \
-										__BSP_LED2_Toggle(); \
-										delay_ms(__TIME__); })
-#endif
+#define __BSP_LED2_Ficker(__TIME__) \
+	do                              \
+	{                               \
+		__BSP_LED2_Toggle();        \
+		delay_ms(__TIME__);         \
+		__BSP_LED2_Toggle();        \
+		delay_ms(__TIME__);         \
+	} while (0)
+
+#define __BSP_LED3_Ficker(__TIME__) \
+	do                              \
+	{                               \
+		__BSP_LED3_Toggle();        \
+		delay_ms(__TIME__);         \
+		__BSP_LED3_Toggle();        \
+		delay_ms(__TIME__);         \
+	} while (0)
 #endif
 
 #ifdef __BSP_MCU_NUCLEO_H7A3ZIQ
@@ -75,50 +126,88 @@ extern "C" {
 #define __BSP_LED2_Toggle() HAL_GPIO_TogglePin(BSP_LED_Port2, BSP_LED_PIN2)
 #define __BSP_LED3_Toggle() HAL_GPIO_TogglePin(BSP_LED_Port3, BSP_LED_PIN3)
 
-/* Macor Function */
+/* Macro Function */
 #if __RTOS_RTTHREAD_ENABLED
-#define __BSP_LED1_Ficker(__TIME__) ({	__BSP_LED1_Toggle(); 		\
-										rt_thread_mdelay(__TIME__); \
-										__BSP_LED1_Toggle(); 		\
-										rt_thread_mdelay(__TIME__); })
+#define __BSP_LED1_Ficker(__TIME__) \
+	do                              \
+	{                               \
+		__BSP_LED1_Toggle();        \
+		rt_thread_mdelay(__TIME__); \
+		__BSP_LED1_Toggle();        \
+		rt_thread_mdelay(__TIME__); \
+	} while (0)
 
-#define __BSP_LED2_Ficker(__TIME__) ({	__BSP_LED2_Toggle(); 		\
-										rt_thread_mdelay(__TIME__);	\
-										__BSP_LED2_Toggle(); 		\
-										rt_thread_mdelay(__TIME__); })
+#define __BSP_LED2_Ficker(__TIME__) \
+	do                              \
+	{                               \
+		__BSP_LED2_Toggle();        \
+		rt_thread_mdelay(__TIME__); \
+		__BSP_LED2_Toggle();        \
+		rt_thread_mdelay(__TIME__); \
+	} while (0)
 
-#define __BSP_LED3_Ficker(__TIME__) ({	__BSP_LED3_Toggle(); 		\
-										rt_thread_mdelay(__TIME__); \
-										__BSP_LED3_Toggle(); 		\
-										rt_thread_mdelay(__TIME__); })
+#define __BSP_LED3_Ficker(__TIME__) \
+	do                              \
+	{                               \
+		__BSP_LED3_Toggle();        \
+		rt_thread_mdelay(__TIME__); \
+		__BSP_LED3_Toggle();        \
+		rt_thread_mdelay(__TIME__); \
+	} while (0)
 #elif __RTOS_FREERTOS_ENABLED
-#define __BSP_LED1_Ficker(__TIME__) ({	__BSP_LED1_Toggle(); 		\
-										vTaskDelay(__TIME__); 	    \
-										__BSP_LED1_Toggle(); 		\
-										vTaskDelay(__TIME__); })
-#define __BSP_LED2_Ficker(__TIME__) ({	__BSP_LED2_Toggle(); 		\
-										vTaskDelay(__TIME__); 	    \
-										__BSP_LED2_Toggle(); 		\
-										vTaskDelay(__TIME__); })
-#define __BSP_LED3_Ficker(__TIME__) ({	__BSP_LED3_Toggle(); 		\
-										vTaskDelay(__TIME__); 	    \
-										__BSP_LED3_Toggle(); 		\
-										vTaskDelay(__TIME__); })
+#define __BSP_LED1_Ficker(__TIME__) \
+	do                              \
+	{                               \
+		__BSP_LED1_Toggle();        \
+		vTaskDelay(__TIME__);       \
+		__BSP_LED1_Toggle();        \
+		vTaskDelay(__TIME__);       \
+	} while (0)
+
+#define __BSP_LED2_Ficker(__TIME__) \
+	do                              \
+	{                               \
+		__BSP_LED2_Toggle();        \
+		vTaskDelay(__TIME__);       \
+		__BSP_LED2_Toggle();        \
+		vTaskDelay(__TIME__);       \
+	} while (0)
+
+#define __BSP_LED3_Ficker(__TIME__) \
+	do                              \
+	{                               \
+		__BSP_LED3_Toggle();        \
+		vTaskDelay(__TIME__);       \
+		__BSP_LED3_Toggle();        \
+		vTaskDelay(__TIME__);       \
+	} while (0)
 #else
-#define __BSP_LED1_Ficker(__TIME__) ({	__BSP_LED1_Toggle(); \
-										delay_ms(__TIME__);  \
-										__BSP_LED1_Toggle(); \
-										delay_ms(__TIME__); })
+#define __BSP_LED1_Ficker(__TIME__) \
+	do                              \
+	{                               \
+		__BSP_LED1_Toggle();        \
+		delay_ms(__TIME__);         \
+		__BSP_LED1_Toggle();        \
+		delay_ms(__TIME__);         \
+	} while (0)
 
-#define __BSP_LED2_Ficker(__TIME__) ({	__BSP_LED2_Toggle(); \
-										delay_ms(__TIME__);  \
-										__BSP_LED2_Toggle(); \
-										delay_ms(__TIME__); })
+#define __BSP_LED2_Ficker(__TIME__) \
+	do                              \
+	{                               \
+		__BSP_LED2_Toggle();        \
+		delay_ms(__TIME__);         \
+		__BSP_LED2_Toggle();        \
+		delay_ms(__TIME__);         \
+	} while (0)
 
-#define __BSP_LED3_Ficker(__TIME__) ({	__BSP_LED3_Toggle(); \
-										delay_ms(__TIME__);  \
-										__BSP_LED3_Toggle(); \
-										delay_ms(__TIME__); })
+#define __BSP_LED3_Ficker(__TIME__) \
+	do                              \
+	{                               \
+		__BSP_LED3_Toggle();        \
+		delay_ms(__TIME__);         \
+		__BSP_LED3_Toggle();        \
+		delay_ms(__TIME__);         \
+	} while (0)
 #endif
 #endif
 
@@ -128,25 +217,36 @@ extern "C" {
 
 #define __BSP_LED_Toggle() HAL_GPIO_TogglePin(BSP_LED_Port, BSP_LED_PIN)
 #if __RTOS_RTTHREAD_ENABLED
-#define __BSP_LED_Ficker(__TIME__) ({	__BSP_LED_Toggle(); 		\
-										rt_thread_mdelay(__TIME__); \
-										__BSP_LED_Toggle(); 		\
-										rt_thread_mdelay(__TIME__); })
+#define __BSP_LED_Ficker(__TIME__)  \
+	do                              \
+	{                               \
+		__BSP_LED_Toggle();         \
+		rt_thread_mdelay(__TIME__); \
+		__BSP_LED_Toggle();         \
+		rt_thread_mdelay(__TIME__); \
+	} while (0)
 #elif __RTOS_FREERTOS_ENABLED
-#define __BSP_LED_Ficker(__TIME__) ({	__BSP_LED_Toggle(); 		\
-										vTaskDelay(__TIME__); 	    \
-										__BSP_LED_Toggle(); 		\
-										vTaskDelay(__TIME__); })	
+#define __BSP_LED_Ficker(__TIME__) \
+	do                             \
+	{                              \
+		__BSP_LED_Toggle();        \
+		vTaskDelay(__TIME__);      \
+		__BSP_LED_Toggle();        \
+		vTaskDelay(__TIME__);      \
+	} while (0)
 #else
-#define __BSP_LED_Ficker(__TIME__) ({	__BSP_LED_Toggle(); 		\
-										delay_ms(__TIME__); 		\
-										__BSP_LED_Toggle(); 		\
-										delay_ms(__TIME__); })
+#define __BSP_LED_Ficker(__TIME__) \
+	do                             \
+	{                              \
+		__BSP_LED_Toggle();        \
+		delay_ms(__TIME__);        \
+		__BSP_LED_Toggle();        \
+		delay_ms(__TIME__);        \
+	} while (0)
 #endif
 #endif
 
-void BSP_LED_Init(void);
-
+	void BSP_LED_Init(void);
 
 #ifdef __cplusplus
 }
